17c3c7f38c10f793442afe8693875062f863194e
Avaer Kazmer <a@modules.io>
2020-05-15 16:35:16 -0400

    Bugfix index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express');
 const app = express();
 app.use(express.static(__dirname));
 
-https.createServer(app)
+http.createServer(app)
   .listen(3000);
 
 console.log(`http://127.0.0.1:3000`);
